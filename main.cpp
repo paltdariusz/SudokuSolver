845c1fde928cc0b8f0ed5be09f3f204cd3b7bdff
paltdariusz <paltdariusz@gmail.com>
2023-01-22 21:45:58 +0100

    MPI version

--- main.cpp
+++ main.cpp
@@ -1,76 +1,110 @@
 #include <iostream>
-#include <array>
-#include <omp.h>
+#include <mpi.h>
+#include <vector>
 #include <chrono>
 
 class Sudoku {
-private:
-    std::array<std::array<int, 9>, 9> grid;
-
 public:
-    // Konstruktor
     Sudoku() {
+        // Initialize grid with all zeroes
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 grid[i][j] = 0;
             }
         }
+
+        // Initialize MPI
+        int provided;
+        MPI_Init_thread(NULL, NULL, MPI_THREAD_FUNNELED, &provided);
     }
 
-    // Metoda do ustawiania wartości na planszy
     void setValue(int row, int col, int value) {
         grid[row][col] = value;
     }
 
-    void printSolution() {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                std::cout << grid[row][col] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    // Metoda do rozwiązywania problemu
     bool solve() {
-        bool solved = false;
-#pragma omp parallel
-        {
-#pragma omp single
-            {
-                solved = solve(0, 0);
-            }
-        }
+        int rank, num_procs;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+        // Start the solving process
+        bool solved = solve(rank, num_procs, 0, 0);
+        MPI_Finalize();
         return solved;
     }
 
+    void printSolution() {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        if (rank == 0) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    std::cout << grid[i][j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+
 private:
-    // Metoda rekurencyjna do rozwiązywania problemu
-    bool solve(int row, int col) {
-        // Jeśli wszystkie pola zostały wybrane, to znaleziono rozwiązanie
+    // 2D grid to store the puzzle
+    int grid[9][9];
+
+    // Helper method to check if a given value is valid for a given cell
+    bool isValid(int row, int col, int value) {
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (grid[row][i] == value || grid[i][col] == value) {
+                return false;
+            }
+        }
+
+        // Check 3x3 sub-grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i + startRow][j + startCol] == value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool solve(int rank, int num_procs, int row, int col) {
+        // If all cells have been filled, return true
         if (row == 9) {
             return true;
         }
 
-        // Przeskakiwanie zapełnionych pól
+        // If cell is already filled, move to the next cell
         if (grid[row][col] != 0) {
             if (col == 8) {
-                return solve(row + 1, 0);
-            } else {
-                return solve(row, col + 1);
+                if (solve(rank, num_procs, row + 1, 0)) {
+                    return true;
+                }
             }
+            else {
+                if (solve(rank, num_procs, row, col + 1)) {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        // Próba wstawienia każdej liczby z zakresu 1-9
-        for (int i = 1; i <= 9; i++) {
-            if (isValid(row, col, i)) {
-                grid[row][col] = i;
+        // Try all possible values for the current cell
+        int start = rank;
+        for (int value = 1; value <= 9; value += num_procs) {
+            if (isValid(row, col, value)) {
+                grid[row][col] = value;
                 if (col == 8) {
-                    if (solve(row + 1, 0)) {
+                    if (solve(rank, num_procs, row + 1, 0)) {
                         return true;
                     }
-                } else {
-                    if (solve(row, col + 1)) {
+                }
+                else {
+                    if (solve(rank, num_procs, row, col + 1)) {
                         return true;
                     }
                 }
@@ -79,86 +113,42 @@ private:
         }
         return false;
     }
-
-    bool isValid(int row, int col, int value) {
-        // Sprawdzanie czy podana wartość już występuje w wierszu
-        if (checkRow(row, value)) {
-            return false;
-        }
-        // Sprawdzanie czy podana wartość już występuje w kolumnie
-        if (checkCol(col, value)) {
-            return false;
-        }
-        // Sprawdzanie czy podana wartość już występuje w kwadracie 3x3
-        if (checkSquare(row, col, value)) {
-            return false;
-        }
-        return true;
-    }
-
-    bool checkRow(int row, int value) {
-        for (int col = 0; col < 9; col++) {
-            if (grid[row][col] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool checkCol(int col, int value) {
-        for (int row = 0; row < 9; row++) {
-            if (grid[row][col] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool checkSquare(int row, int col, int value) {
-        int squareRow = row - row % 3;
-        int squareCol = col - col % 3;
-        for (int i = squareRow; i < squareRow + 3; i++) {
-            for (int j = squareCol; j < squareCol + 3; j++) {
-                if (grid[i][j] == value) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 };
 
-int main() {
-    // Tworzenie obiektu klasy Sudoku
+int main(int argc, char** argv) {
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Create a Sudoku object
     Sudoku sudoku;
 
-    // Wprowadzanie danych do planszy
+    // Set the initial state of the puzzle
     sudoku.setValue(0, 0, 8);
     sudoku.setValue(0, 1, 5);
-    sudoku.setValue(0, 7, 2);
-    sudoku.setValue(0, 8, 9);
-    sudoku.setValue(1, 0, 7);
-    sudoku.setValue(1, 1, 2);
-    sudoku.setValue(2, 2, 4);
-    sudoku.setValue(3, 3, 1);
-    sudoku.setValue(3, 4, 2);
-    sudoku.setValue(4, 6, 3);
-    sudoku.setValue(4, 7, 4);
-    sudoku.setValue(5, 8, 2);
-    sudoku.setValue(6, 8, 6);
-    sudoku.setValue(7, 8, 8);
-    sudoku.setValue(8, 0, 6);
-    sudoku.setValue(8, 4, 5);
-    sudoku.setValue(8,8,1);
-    // Rozwiązywanie problemu
-    if (sudoku.solve()) {
-        // Jeśli rozwiązanie znalezione, wyświetlenie rozwiązania
-        sudoku.printSolution();
+    //...
 
-    }
-    else {
-        std::cout << "Nie znaleziono rozwiazania." << std::endl;
-    }
+    // Record the start time
+    auto start_time = std::chrono::high_resolution_clock::now();
 
+    // Solve the puzzle
+    bool solved = sudoku.solve();
+
+    // Record the end time
+    auto end_time = std::chrono::high_resolution_clock::now();
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        if (solved) {
+            // Print the solution
+            sudoku.printSolution();
+
+            // Calculate and print the time taken to solve the puzzle
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+            std::cout << "Time taken to solve the puzzle: " << duration.count() << " microseconds" << std::endl;
+        } else {
+            std::cout << "No solution found." << std::endl;
+        }
+    }
+    MPI_Finalize();
     return 0;
 }
